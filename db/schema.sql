42d121c4b669ca0bd9ca378a1f051cee28baffcb
KateV <katevoronina128@gmail.com>
2026-07-13 17:06:56 +0400

    fix db

--- db/schema.sql
+++ db/schema.sql
@@ -3,30 +3,41 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS firmware (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
     size INTEGER NOT NULL,
-    mod_time DATETIME NOT NULL,
+    version TEXT NOT NULL,
     sha256 TEXT NOT NULL,
     path TEXT NOT NULL,
+    is_current INTEGER NOT NULL DEFAULT 0 CHECK(is_current IN (0,1)),
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_firmware_name_version
+ON firmware(name, version);
 
-CREATE INDEX IF NOT EXISTS idx_firmware_name
-ON firmware(name);
-
+CREATE UNIQUE INDEX IF NOT EXISTS idx_firmware_current
+ON firmware(is_current)
+WHERE is_current = 1;
 
 
 CREATE TABLE IF NOT EXISTS flasher (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-    size INTEGER NOT NULL,
-    mod_time DATETIME NOT NULL,
+    name TEXT NOT NULL,
+    os TEXT NOT NULL ,
+    version TEXT NOT NULL,
+    size INTEGER NOT NULL, 
     sha256 TEXT NOT NULL,
     path TEXT NOT NULL,
+    is_current INTEGER NOT NULL DEFAULT 0 CHECK(is_current IN (0,1)),
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+ 
+CREATE UNIQUE INDEX IF NOT EXISTS idx_flasher_name_os_version
+ON flasher(name, os, version);
 
+CREATE INDEX IF NOT EXISTS idx_flasher_os
+ON flasher(os);
 
-CREATE INDEX IF NOT EXISTS idx_flasher_name
-ON flasher(name);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_flasher_os_current
+ON flasher(os)
+WHERE is_current = 1;
